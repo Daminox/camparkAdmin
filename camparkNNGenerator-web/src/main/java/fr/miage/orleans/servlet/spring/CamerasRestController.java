2fae80cce528e56211c6675323d08afbc96e0608
Steve <Steve@192.168.1.13>
2013-12-26 18:43:54 +0100

    nouveau ws pour upload image avec nom caméra dans l'url et non dans le body

--- camparkNNGenerator-web/src/main/java/fr/miage/orleans/servlet/spring/CamerasRestController.java
+++ camparkNNGenerator-web/src/main/java/fr/miage/orleans/servlet/spring/CamerasRestController.java
@@ -5,10 +5,10 @@ import fr.miage.orleans.modele.services.ServiceFluxPhotos;
 import fr.miage.orleans.modele.services.valueobjects.CameraVoFluxPhotos;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +23,13 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 @Controller
 @RequestMapping("/cameras")
 public class CamerasRestController {
+    
+    private static final int STATUS_CODE_BAD_REQUEST = 400;
+    
+    private static final int STATUS_CODE_FORBIDDEN = 403;
 
+    private static final int STATUS_CODE_NOT_FOUND = 404;
+    
     @Autowired
     private ServiceFluxPhotos serviceFluxPhotos;
 
@@ -36,7 +42,7 @@ public class CamerasRestController {
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public @ResponseBody
-    String uploadFile(@RequestParam String nomCamera, @RequestParam MultipartFile file, MultipartHttpServletRequest request) {
+    String uploadFile(@RequestParam String nomCamera, @RequestParam MultipartFile file, MultipartHttpServletRequest request, HttpServletResponse response) {
 	String res;
 	if (nomCamera != null && !nomCamera.isEmpty() && file != null && !file.isEmpty()) {
 	    System.out.println("nomCamera : " + nomCamera);
@@ -45,15 +51,42 @@ public class CamerasRestController {
 	    try {
 		imageCamera = this.serviceFluxPhotos.nouvelleImageCamera(file.getBytes(), nomCamera);
 	    } catch (IOException ex) {
-		Logger.getLogger(CamerasRestController.class.getName()).log(Level.SEVERE, null, ex);
 	    }
 	    if (imageCamera != null) {
 		res = Boolean.toString(true);
 	    } else {
 		res = "Camera inexistante";
+		response.setStatus(STATUS_CODE_NOT_FOUND);
 	    }
 	} else {
 	    res = "Parametres invalides";
+	    response.setStatus(STATUS_CODE_BAD_REQUEST);
+	}
+
+	return res;
+    }
+    
+    @RequestMapping(value = "/upload/{cameraName}", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFile2(@PathVariable String cameraName, @RequestParam MultipartFile file, MultipartHttpServletRequest request, HttpServletResponse response) {
+	String res;
+	if (cameraName != null && !cameraName.isEmpty() && file != null && !file.isEmpty()) {
+	    System.out.println("nomCamera : " + cameraName);
+	    System.out.println("file.getSize() : " + file.getSize());
+	    ImageCamera imageCamera = null;
+	    try {
+		imageCamera = this.serviceFluxPhotos.nouvelleImageCamera(file.getBytes(), cameraName);
+	    } catch (IOException ex) {
+	    }
+	    if (imageCamera != null) {
+		res = Boolean.toString(true);
+	    } else {
+		res = "Camera inexistante";
+		response.setStatus(STATUS_CODE_NOT_FOUND);
+	    }
+	} else {
+	    res = "Parametres invalides";
+	    response.setStatus(STATUS_CODE_BAD_REQUEST);
 	}
 
 	return res;
